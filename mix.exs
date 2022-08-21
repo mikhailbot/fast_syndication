c19bcc469229b8947a95528b5ba5d8062f7e795f
Mikhail Delport <mikhail@delport.ca>
2022-08-21 12:20:23 -0400

    Update version
    Breaking change

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule FastSyndication.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/mikhailbot/fast_syndication"
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
